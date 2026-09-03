5874ab6a0f03ed0cdc368a65a07d90c5eaab4fe6
frankvision <132785044+frankvision@users.noreply.github.com>
2026-09-03 17:16:13 +0200

    Update animation targets in main.js

--- main.js
+++ main.js
@@ -2118,7 +2118,7 @@ moTl.fromTo(pairsMobile[2], {
   }
 },"<");
 
-moTl.fromTo([pairsMobile[3],pairsMobile[10]],{
+moTl.fromTo([pairsMobile[7],pairsMobile[14]],{
    autoAlpha: 0,
   scale: 0.2,
 },
